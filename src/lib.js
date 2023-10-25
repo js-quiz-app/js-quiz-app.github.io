47bb9798e61f220b2cb507571481e5021757c296
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-10-25 13:44:28 +0300

    Updated lib

--- src/lib.js
+++ src/lib.js
@@ -1,9 +1,9 @@
-import page from '//unpkg.com/page/page.mjs';
-import { html, render } from '//unpkg.com/lit-html?module';
-import { until } from '//unpkg.com/lit-html/directives/until?module';
-import { cache } from '//unpkg.com/lit-html/directives/cache?module';
-import { classMap } from '//unpkg.com/lit-html/directives/class-map?module';
-import { styleMap } from '//unpkg.com/lit-html/directives/style-map?module';
+import page from '../node_modules/page/page.mjs';
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { until } from '../node_modules/lit-html/directives/until.js';
+import { cache } from '../node_modules/lit-html/directives/cache.js';
+import { classMap } from '../node_modules/lit-html/directives/class-map.js';
+import { styleMap } from '../node_modules/lit-html/directives/style-map.js';
 
 
 export {
