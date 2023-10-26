aed6a2d44c581299cb6ef00c7f23849dd33181da
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-10-26 15:46:07 +0300

    Update notFound.js

--- src/views/notFound.js
+++ src/views/notFound.js
@@ -1,12 +1,12 @@
-import { html, until } from '../lib.js';
+import { html, until, styleMap } from '../lib.js';
 
 const notFoundTemplate = () => html`
 <section id="details">
     <div class="pad-large alt-page">
         <article class="details">
-            <h1>404</h1>
+            <h1 style=${styleMap({'font-size': '48px'})}>404</h1>
 
-            <p class="quiz-desc">Page Not Found</p>
+            <p class="quiz-desc" style=${styleMap({'text-align': 'center', 'font-size': '24px'})}>Page Not Found</p>
         </article>
     </div>
 </section>`;
