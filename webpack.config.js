c23cf67591d74a84e31cb0fc2ee9d39e8b7d415e
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-10-26 17:34:43 +0300

    Added webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+import path from 'path';
+
+export default {
+    entry: './src/app.js',  // Entry point of your application
+    output: {
+        path: path.resolve(__dirname, 'dist'),  // Output directory
+        filename: 'bundle.js',  // Output filename
+    },
+    module: {
+        rules: [
+            // Define loaders for different file types (e.g., Babel for JavaScript)
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+};
